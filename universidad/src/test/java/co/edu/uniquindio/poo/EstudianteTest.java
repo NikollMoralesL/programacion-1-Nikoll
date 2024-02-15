7ff640a11beddeeea8bec6423277cdf508304a91
NikollMoralesL <nikoll.moralesl@uqvirtual.edu.co>
2024-02-15 16:45:08 -0500

    nuevo proyecto
    este cambio muestra la creacion del proyecto inicial

---
+++ universidad/src/test/java/co/edu/uniquindio/poo/EstudianteTest.java
@@ -0,0 +1,30 @@
+/**
+ * Clase para probar el funcionamiento del código
+ * @author Área de programación UQ
+ * @since 2023-08
+ * 
+ * Licencia GNU/GPL V3.0 (https://raw.githubusercontent.com/grid-uq/poo/main/LICENSE) 
+ */
+package co.edu.uniquindio.poo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.logging.Logger;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EstudianteTest {
+    private static final Logger LOG = Logger.getLogger(EstudianteTest.class.getName());
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        LOG.info("Iniciado test shouldAnswerWithTrue");
+        assertTrue(true);
+        LOG.info("Finalizando test shouldAnswerWithTrue");
+        ljyfkjffkjhf
+    }
+}
