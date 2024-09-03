e273e91925c1f9fe485b24a4834a0a27494b9c3f
NikollMoralesL <159401528+NikollMoralesL@users.noreply.github.com>
2024-09-02 21:02:04 -0500

    Add files via upload

---
+++ agenda/src/main/java/co/edu/uniquindio/poo/Categoria.java
@@ -0,0 +1,6 @@
+package co.edu.uniquindio.poo;
+public enum Categoria {
+    OFICINA, FIESTA, AMIGOS, FAMILIA
+
+    }
+
---
+++ agenda/src/main/java/co/edu/uniquindio/poo/Grupo.java
@@ -0,0 +1,82 @@
+package co.edu.uniquindio.poo;
+import java.util.LinkedList;
+
+public class Grupo {
+    private String nombre; 
+    private Categoria categoria;
+    private LinkedList<Contacto> contactos;
+
+    public Grupo(String nombre){
+        this.nombre = nombre;
+        contactos = new LinkedList<>();
+
+    }
+    
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getNombnre() {
+        return nombre;
+    }
+
+    public void setNombnre(String nombnre) {
+        this.nombre = nombnre;
+    }
+
+  
+    public static void mostrarMensaje (String mensaje){
+        System.out.println(mensaje);
+    }
+
+    public LinkedList<Contacto> getContactos() {
+        return contactos;
+    }
+
+    public void setContactos(LinkedList<Contacto> contactos) {
+        this.contactos = contactos;
+    }
+
+    //
+    public void agregarContacto(Contacto contacto){
+        if (validarNombreTelefono(contacto.getNombre(),contacto.getTelefono())){
+            mostrarMensaje("Error, el nombre"+contacto.getNombre()+ "y" +contacto.getTelefono()+ "ya esta creado");
+        }else{
+            contactos.add(contacto);
+        }
+    }
+
+    public boolean validarNombreTelefono(String nombre, String telefono){
+            for(Contacto contacto:contactos){
+                if(contacto.getNombre().equals(nombre)&& contacto.getTelefono().equals(telefono)){
+                    return true;
+                }
+            }
+            return false;
+    }
+
+    public void eliminarContacto(String nombre){
+        for (Contacto contacto: contactos){
+            if(contacto.getNombre().equals(nombre)){
+                contactos.remove(contacto);
+                break;
+                }
+            }
+    }
+    //metodo para actu
+     public void actualizarContacto(String nombre, String alias, String direccion){
+        for (Contacto contacto: contactos){    
+            if (contacto.getNombre().equals(nombre)){
+                contacto.setAlias(alias);
+                contacto.setDireccion(direccion);
+                break;
+
+            }
+    
+        }
+    }
+}
